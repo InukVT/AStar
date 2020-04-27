ab5fd5e06be79b645a3c8023bf7ab90afb195337
Bastian Inuk Christensen <bastian@inuk.blog>
2020-04-27 14:11:00 +0200

    Writes to text area

--- src/app/inuk/Controller.java
+++ src/app/inuk/Controller.java
@@ -8,6 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 import javax.print.attribute.standard.Destination;
@@ -25,14 +26,53 @@ public class Controller {
     @FXML
     private Button printPath;
 
+    @FXML
+    private TextArea textArea;
+
     public void placeHolder(){
         heuristics.getItems().setAll(Heuristics.values());
+        heuristics.setValue(Heuristics.Euclidean);
         startVertex.setItems(FXCollections.observableArrayList(Main.MyMaze.getVertices()));
+        startVertex.setValue(Main.MyMaze.getVertices().get(0));
         destination.setItems(FXCollections.observableArrayList(Main.MyMaze.getVertices()));
+
+        destination.setOnAction(e -> {
+            System.out.println("Start is " + startVertex.getValue());
+            System.out.println("Destination is "+ destination.getValue());
+            String path = aStar(startVertex.getValue(), destination.getValue(), heuristics.getValue());
+            System.out.println(path);
+            textArea.setText(path);
+        });
         //printPath.setOnAction(e -> );
     }
     @FXML
     public void initialize(){
         placeHolder();
+    }
+
+    String aStar(Vertex from, Vertex to, Heuristics heuristics)
+    {
+        String path = "";
+            if (Main.MyMaze.A_Star(from, to, heuristics)) {
+                path += "Found a path\n";
+                Vertex pvertex = from;
+                Stack<Vertex> Path = new Stack<>();
+            int limit = 0;
+            while (pvertex != null) {
+                Path.push(pvertex);
+                pvertex = pvertex.getPrev();
+            }
+            if (!Path.isEmpty())
+                limit = Path.size();
+            for (int i = 0; i < limit - 1; i++)
+                path += Path.pop().getid() + " - > ";
+            if (limit > 0)
+                path += Path.pop() ;
+
+        } else {
+            path += "DID NOT FIND A PATH!!";
+        }
+        path += "\n";
+        return path;
     }
 }
